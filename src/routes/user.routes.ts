caf4faba6040599589df57f89585c63b53778bfc
ThinhgiaDoan <hopee.giathinh@gmail.com>
2023-10-22 17:40:35 +0700

    check validator

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { registerController } from '~/controllers/user.controllers'
+import { registerValidator } from '~/middlewares/users.middlewares'
 
 const usersRouter = Router()
 
 usersRouter.post('/login')
 
-usersRouter.post('/register', registerController)
+usersRouter.post('/register', registerValidator, registerController)
 
 export default usersRouter
